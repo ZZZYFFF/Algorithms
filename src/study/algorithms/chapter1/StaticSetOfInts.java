1400008b610fade0ca438c12bf445a9ea5dee00d
zzzyfff <963991577@qq.com>
2020-05-22 22:58:40 +0800

    StaticSetOfInts

---
+++ src/study/algorithms/chapter1/StaticSetOfInts.java
@@ -0,0 +1,59 @@
+package study.algorithms.chapter1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * @author zyf
+ */
+public class StaticSetOfInts {
+    private int[] a;
+
+    public StaticSetOfInts(int[] keys) {
+        a = new int[keys.length];
+        for (int i = 0;i<keys.length;i++){
+            a[i] = keys[i];
+        }
+        Arrays.sort(a);
+    }
+
+    public boolean contains(int key){
+        return rank(key) !=-1;
+    }
+
+    /**
+     *
+     * 二分查找
+     */
+    private int rank(int key) {
+        int lo = 0;
+        int hi = a.length-1;
+        while (lo<=hi){
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            } else if (key > a[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whiteList = In.readInts(args[0]);
+        StaticSetOfInts set = new StaticSetOfInts(whiteList);
+        Arrays.sort(whiteList);
+        while (!StdIn.isEmpty()) {
+            //读取的键，如果不存在白名单中则将其打印
+            int key = StdIn.readInt();
+            if (!set.contains(key)) {
+                StdOut.println(key);
+            }
+        }
+    }
+}
--- src/study/algorithms/chapter1/TestVisualAccumulator.java
+++ src/study/algorithms/chapter1/TestVisualAccumulator.java
@@ -11,9 +11,9 @@ public class TestVisualAccumulator {
      * @param args 2000
      */
     public static void main(String[] args) {
-        int T = Integer.parseInt(args[0]);
-        VisualAccumulator accumulator = new VisualAccumulator(T, 1.0);
-        for (int t = 0; t < T; t++) {
+        int in = Integer.parseInt(args[0]);
+        VisualAccumulator accumulator = new VisualAccumulator(in, 1.0);
+        for (int t = 0; t < in; t++) {
             accumulator.addDataValue(StdRandom.uniform());
         }
         StdOut.println(accumulator);
