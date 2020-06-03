a025f6c5d5c8c374f9e9117ee09c8c70c8958341
zzzyfff <963991577@qq.com>
2020-06-04 00:21:32 +0800

    2.1   TestSorts.java 算法测试

---
+++ src/study/algorithms/chapter2/elementarysorts/TestSorts.java
@@ -0,0 +1,19 @@
+package study.algorithms.chapter2.elementarysorts;
+
+import edu.princeton.cs.algs4.In;
+
+/**
+ * 测试排序算法
+ *
+ * java -classpath . study.algorithms.chapter2.elementarysorts.TestSorts < tiny.txt
+ * @author zyf
+ */
+public class TestSorts {
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        Sort sort = new Selection();
+        sort.sort(a);
+        assert Sort.isSorted(a);
+        Sort.show(a);
+    }
+}
