ba4299be5b8728e8a57d2057b9e4c97dd807facf
zzzyfff <963991577@qq.com>
2020-06-11 23:34:48 +0800

    归并 排序

--- src/study/algorithms/chapter2/mergesort/Merge.java
+++ src/study/algorithms/chapter2/mergesort/Merge.java
@@ -8,8 +8,42 @@ import study.algorithms.chapter2.elementarysorts.Sort;
  * @author zyf
  */
 public class Merge implements Sort {
+    /**
+     * 归并所需的辅助数组
+     */
+    public static Comparable[] aux;
+
     @Override
     public void sort(Comparable[] a) {
 
     }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[k];
+            } else if (Sort.less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
 }
